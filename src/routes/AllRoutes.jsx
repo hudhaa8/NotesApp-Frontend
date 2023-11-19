8e480208ab09e3e5d1c6e0c679214949320c8bf9
hudhaa8 <noorulcse@gmail.com>
2023-11-19 17:34:43 +0530

    chnage

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,4 +1,5 @@
-import { Route,Routes } from "react-router-dom";
+// import { Route,Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
 import NotesPage from "../pages/NotesPage";
